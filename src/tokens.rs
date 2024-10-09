c705ce436045ff542941249f58e0576802fb9fbe
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2024-10-09 19:18:33 +0200

    add: helpers for token types

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,4 +1,6 @@
 use std::cmp::PartialEq;
+use crate::ast::{HuleExpressionResultExt, Operator};
+use crate::parser::{AstParser, AstParserError};
 
 #[derive(PartialEq, Copy, Clone, Debug,)]
 pub enum TokenType {
@@ -28,7 +30,39 @@ pub enum TokenType {
     Minus = 23,
     Divide = 24,
     Multiply = 25,
-    ConstStringValue = 26,
+}
+
+impl TokenType {
+    pub fn to_string(&self) -> String {
+        match self {
+            TokenType::Undefined => "Undefined".to_string(),
+            TokenType::Unknown => "Unknown".to_string(),
+            TokenType::Identifier => "Identifier".to_string(),
+            TokenType::ConstIntegerExpression => "ConstIntegerExpression".to_string(),
+            TokenType::ConstStringExpression => "ConstStringExpression".to_string(),
+            TokenType::BracketOpen => "BracketOpen".to_string(),
+            TokenType::BracketClose => "BracketClose".to_string(),
+            TokenType::CurlyBracketOpen => "CurlyBracketOpen".to_string(),
+            TokenType::CurlyBracketClose => "CurlyBracketClose".to_string(),
+            TokenType::SquareBracketOpen => "SquareBracketOpen".to_string(),
+            TokenType::SquareBracketClose => "SquareBracketClose".to_string(),
+            TokenType::Assign => "Assign".to_string(),
+            TokenType::Equal => "Equal".to_string(),
+            TokenType::NotEqual => "NotEqual".to_string(),
+            TokenType::GreaterThan => "GreaterThan".to_string(),
+            TokenType::LowerThan => "LowerThan".to_string(),
+            TokenType::GreaterEqualThan => "GreaterEqualThan".to_string(),
+            TokenType::LowerEqualThan => "LowerEqualThan".to_string(),
+            TokenType::And => "And".to_string(),
+            TokenType::Or => "Or".to_string(),
+            TokenType::Comma => "Comma".to_string(),
+            TokenType::Semicolon => "Semicolon".to_string(),
+            TokenType::Plus => "Plus".to_string(),
+            TokenType::Minus => "Minus".to_string(),
+            TokenType::Divide => "Divide".to_string(),
+            TokenType::Multiply => "Multiply".to_string(),
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -39,6 +73,20 @@ pub struct Token {
     token_end : u32
 }
 
+impl HuleExpressionResultExt for Result<Operator, AstParserError> {
+    fn or_reset(self, program : &mut AstParser, index : usize) -> Self
+    {
+        match &self {
+            Ok(_) => {
+                self
+            }
+            Err(_) => {
+                program.tokens.set_current_token_index(index);
+                self
+            }
+        }
+    }
+}
 
 impl Token {
     pub fn new(value : String, token_start : u32, token_end : u32) -> Token {
@@ -76,7 +124,7 @@ impl Token {
             "*" => TokenType::Multiply,
             _ => {
                 if token.starts_with('"') && token.ends_with('"') {
-                    TokenType::ConstStringValue
+                    TokenType::ConstStringExpression
                 } else if let Ok(int) = &self.value.parse::<i32>() {
                     TokenType::ConstIntegerExpression
                 } else if let Ok(int) = &self.value.parse::<i32>() {
