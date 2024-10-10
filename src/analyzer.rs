27d1c87cdd889e72718da62a99f2edadc9d548dd
Milos Stanojevic <37949266+pointermess@users.noreply.github.com>
2024-10-10 18:54:16 +0200

    feat: initial analyzer

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,9 +1,38 @@
+use crate::ast::{FunctionCallIterator, HuleFuncCall, HuleProgramAst, HuleStatement};
 use crate::program::HuleProgram;
 
+pub enum AnalyzerError {
+    EntryPointMissing,
+    FunctionRedefined(String),
+}
+
 pub struct Analyzer {
-    program: HuleProgram
+    program: HuleProgramAst
 }
 
 impl Analyzer {
+    pub fn new(program: HuleProgramAst) -> Analyzer {
+        Analyzer {
+            program
+        }
+    }
 
+    pub fn get_function_by_name(&self, name: &str) -> Option<HuleFuncCall> {
+        self.program.body.items
+            .iter_function_calls()
+            .find(|p| p.name == name)
+    }
+
+    pub fn contains_function(&self, name: &str) -> bool {
+        self.get_function_by_name(name)
+            .is_some()
+    }
+
+    fn analyze_unique_functions() -> Result<(), AnalyzerError> {
+        Err(AnalyzerError::EntryPointMissing)
+    }
+
+    pub fn analyze(&mut self) {
+
+    }
 }
